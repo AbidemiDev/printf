47fe19ff11bf3cba31d78e2e532b01f7adce6b4d
AbidemiDev <obamisaiah8@gmail.com>
2023-10-20 12:44:20 -0700

    Printf

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format.
+ * _printf - Receives the main string and all the necessary parameters to
  * print a formated string
  * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
